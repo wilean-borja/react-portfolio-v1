9a35350bfe789dc379d258f20e10511cfff42e60
Wilean Jose Macalalad <61538917+wileanborja@users.noreply.github.com>
2021-12-03 11:59:32 +0800

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,31 @@
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            your logo here
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>123454878</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>sksdan@user.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
