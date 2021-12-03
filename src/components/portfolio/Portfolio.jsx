3d4d1a6821422da107e22835c115490a37404bd5
Wilean Jose Macalalad <61538917+wileanborja@users.noreply.github.com>
2021-12-03 18:55:21 +0800

    intro section done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,22 @@
 import "./portfolio.scss";
 
 export default function Portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li>Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Architecture</li>
+        <li>Design</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
